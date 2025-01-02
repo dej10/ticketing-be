16949aade97c8184de01f04679697a5c330a44c1
Ayodeji Alarape <ayodeji775@gmail.com>
2025-01-02 16:15:06 +0100

    chore : add staging workflow

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,5 +12,12 @@ module.exports = {
       script: 'app.js',
       port: process.env.PORT,
     },
+      {
+      name: 'ticketing-staging.dev',
+      exec_mode: 'fork',
+      instances: '1',
+      script: 'app.js',
+      port: process.env.PORT,
+    },
   ],
 }
