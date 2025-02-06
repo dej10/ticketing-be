0e547e636f8b464f92ed375d6948c554928abacb
Ayodeji Alarape <46384451+dej10@users.noreply.github.com>
2025-02-06 11:50:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const noFileUpload = multer().none()
 const fileUpload = multer().any()
 
 const corsOpts = {
-  origin: ['http://localhost:3002', 'https://ticketing-fe.pages.dev'],
+  origin: ['http://localhost:3002', 'https://ticketing-fe.pages.dev' , 'https://axxis.wearcheck.co'],
 
   methods: ['GET', 'POST', 'DELETE'],
 
